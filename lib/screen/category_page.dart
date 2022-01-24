f65f2c6a130625fa67ae4f2ee4e834f48bfd1ff4
origogi <rlawjdxo88@gmail.com>
2022-01-25 08:09:35 +0900

    Improve web loading time

--- lib/screen/category_page.dart
+++ lib/screen/category_page.dart
@@ -1,41 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:jm_promise/component/Banner.dart';
+import 'package:jm_promise/component/PromiseCard.dart';
 
 class CategoryPage extends StatelessWidget {
-
-  List<Widget> cards;
+  List<String> cardImgUris;
   BannerWidget banner;
-  CategoryPage(this.cards, this.banner);
+  CategoryPage(this.cardImgUris, this.banner);
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-          body: Column(
-            children: [
-              InkWell(
-                onTap: () {
-                  Navigator.of(context).pop();
-                },
-                child: Image.asset('assets/header.jpg', fit: BoxFit.cover),
-              ),
-              banner,
-              Container(
-                height: 10,
-              ),
-              Expanded(
-                  child: PageView(
-                      children: cards,
-                      controller: PageController(
-                        viewportFraction: 0.8,
-                        initialPage: 0,
-                      ))),
-              Container(
-                height: 10,
-              ),
-            ],
+      body: Column(
+        children: [
+          InkWell(
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+            child: Image.asset('assets/header.jpg', fit: BoxFit.cover),
           ),
-        ));
+          banner,
+          Container(
+            height: 10,
+          ),
+          Expanded(
+            child: PageView.builder(
+                controller: PageController(
+                  viewportFraction: 0.8,
+                  initialPage: 0,
+                ),
+                itemBuilder: (context, index) {
+                  return PromiseCard(cardImgUris[index]);
+                }),
+          ),
+
+          Container(
+            height: 10,
+          ),
+        ],
+      ),
+    ));
   }
 }
-
